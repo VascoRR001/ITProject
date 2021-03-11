b1cad839ba226d70775401c6e10110feb3da2f64
raksha shrees <raksha.shrees6@gmail.com>
2021-03-11 14:23:26 +0100

    test1

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
         </div>
         <div class=" col-xs-12 col-md-6 text-center second-section">
           <h2><?php echo "Discover Nepal"?></h2>
-          <p class="lead">"Nepal" is one of the fast growing tourism destination</p>
+          <p class="lead"><?php echo "Nepal is one of the fast growing tourism destination"?></p>
           <p class="lead">The ideology is to provide complete information about geographical location, natural beauty and scenary of Nepal. This portal aims to give information about the preparation 
             to be done before visiting Nepal.</p>
             <p class="lead">Sagarmatha National Park, Kathmandu Valley, Chitwan National Park, Lumbini the Birthplace of the Lord Buddha 
