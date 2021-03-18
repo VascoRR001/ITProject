36547bfdf37e94831e9031669a123b7f52de73f5
raksha shrees <raksha.shrees6@gmail.com>
2021-03-18 22:09:00 +0100

    added footer section

--- index.php
+++ index.php
@@ -154,8 +154,39 @@
       </div>
     </div>
   </div>
-
-
+  
+  <!--Footer-->
+  <div id="footer">
+    <div class="container">
+      <div class="row services">
+        <div class="col-xs-12 col-md-4 item section-footer">
+          <h3>About Discover Nepal</h3>
+          <p>We love our country and the Himalayas and we know that there is much more than you can see in a "guidebook". Don't hesitate to talk with us, to ask whatever you want to know, any doubt you have. We enjoy answering your questions and we suggest some options to you. We enjoy guiding you.</p>
+        </div>
+        <div class="col-xs-12 col-md-4 item section-footer">
+          <h3>Contact</h3>
+          <div class="subfooter1">
+            <b>Address :</b> Maitidevi-32, Kathmandu</br>
+            <b>E-mail :</b> info@discovernepal.com.np</br>
+            <b>Telephone :</b> 01-6201274, 01-4421274</br>
+            <b>Cellphone : </b>+977-9851075274</br>
+          </div>    
+        </div>
+        <div class="col-xs-12 col-md-4 item section-footer">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="#Using_Lists_for_Menus">Jungle Safari</a></li>
+            <li><a href="#Using_Lists_for_Menus">Bungy Jump</a></li>
+            <li><a href="#Using_Lists_for_Menus">Mardi Trek</a></li> 
+            <li><a href="#Using_Lists_for_Menus">Everest Base Camp Trek</a></li>
+            <li><a href="#Using_Lists_for_Menus">Pokhara City Tour</a></li>
+            <li><a href="#Using_Lists_for_Menus">Kathmandu City Tour</a></li>
+          </ul> 
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Footer Section Ends here-->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
     integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi"
     crossorigin="anonymous"></script>
@@ -164,5 +195,4 @@
     crossorigin="anonymous"></script>
     
 </body>
-
 </html>
